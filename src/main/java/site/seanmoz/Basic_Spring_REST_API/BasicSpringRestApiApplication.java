3e745f15ff9929aec28f27b1e401f0d45a7d2c86
Morrissey, Sean <seanmorrissey@hotmail.co.uk>
2021-12-07 20:30:56 +0000

    Add skeleton Spring boot application files
    Signed-off-by: Morrissey, Sean <seanmorrissey@hotmail.co.uk>

---
+++ src/main/java/site/seanmoz/Basic_Spring_REST_API/BasicSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package site.seanmoz.Basic_Spring_REST_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/seanmoz/Basic_Spring_REST_API/BasicSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package site.seanmoz.Basic_Spring_REST_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
