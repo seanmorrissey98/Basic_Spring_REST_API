e58c4af1fd64806c7aed66e8000de76059ba0ee5
Morrissey, Sean <seanmorrissey@hotmail.co.uk>
2021-12-07 20:38:07 +0000

    Add basic Greeting POJO
    Signed-off-by: Morrissey, Sean <seanmorrissey@hotmail.co.uk>

---
+++ src/main/java/site/seanmoz/Basic_Spring_REST_API/Greeting.java
@@ -0,0 +1,21 @@
+package site.seanmoz.Basic_Spring_REST_API;
+
+public class Greeting {
+
+    private final long id;
+    private String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
